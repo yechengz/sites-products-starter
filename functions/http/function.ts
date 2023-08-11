511599f77539302cd89ed173115a3deab80c19ef
Ye Cheng Zheng <yechengzh@gmail.com>
2023-08-11 16:33:51 -0400

    rm cache control

--- functions/http/function.ts
+++ functions/http/function.ts
@@ -8,7 +8,6 @@ export async function main() {
         statusCode: 200,
         headers: {
             ...CORS_HEADERS,
-            "Cache-Control": "private",
         },
         body: "test",
     }
